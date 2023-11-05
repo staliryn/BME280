64021adc871d8fb3023512b21786edac015036cb
staliryn <stuartfellipe@gmail.com>
2023-11-05 17:08:26 -0300

    Temperature reading - OK. Missing pressure and humidity reading.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,6 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,13 +49,70 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+float bme280_temp(I2C_HandleTypeDef *hi2c, uint16_t devAdress){
 
+	double var1,var2;
+	float temperature;
+
+	int32_t dig_sample,adc_T;
+	int16_t dig_t2,dig_t3;
+	uint16_t dig_t1;
+
+	uint8_t temperature_data1[] = {0x88,0x89},
+		  temperature_data2[] = {0x8A,0x8B},
+		  temperature_data3[] = {0x8C,0x8D},
+
+		  temperature_stored1[2],
+		  temperature_stored2[2],
+		  temperature_stored3[2],
+
+		  sample_temperature_data[] = {0xFA},
+		  temperature_stored_sample[3];
+
+	HAL_I2C_Master_Transmit(hi2c,devAdress,&sample_temperature_data,1,100);
+	HAL_I2C_Master_Receive(hi2c,devAdress,&temperature_stored_sample,3,100);
+
+	HAL_I2C_Master_Transmit(hi2c, devAdress, &temperature_data1, 1, 100);
+	HAL_I2C_Master_Receive(hi2c, devAdress, &temperature_stored1, 2, 100);
+
+	HAL_I2C_Master_Transmit(hi2c, devAdress, &temperature_data2, 1, 100);
+	HAL_I2C_Master_Receive(hi2c, devAdress, &temperature_stored2, 2, 100);
+
+	HAL_I2C_Master_Transmit(hi2c, devAdress, &temperature_data3, 1, 100);
+	HAL_I2C_Master_Receive(hi2c, devAdress, &temperature_stored3, 2, 100);
+
+
+	dig_t1 = temperature_stored1[1];
+	dig_t1 = (dig_t1 << 8) + temperature_stored1[0];
+
+	dig_t2 = temperature_stored2[1];
+	dig_t2 = (dig_t2 << 8) + temperature_stored2[0];
+
+	dig_t3 = temperature_stored3[1];
+	dig_t3 = (dig_t3 << 8) + temperature_stored3[0];
+
+
+	dig_sample = temperature_stored_sample[0];
+	dig_sample = (dig_sample << 12);
+	dig_sample += (temperature_stored_sample[1] << 4);
+	dig_sample += temperature_stored_sample[0];
+
+	adc_T = dig_sample;
+
+	var1  = (((double)adc_T)/16384.0 - ((double)dig_t1)/1024.0) * ((double)dig_t2);
+	var2  = ((((double)adc_T)/131072.0 - ((double)dig_t1)/8192.0) * (((double)adc_T)/131072.0 - ((double) dig_t1)/8192.0)) * ((double)dig_t3);
+	temperature = (var1 + var2) / 5120;
+
+	return temperature;
+
+}
 /* USER CODE END 0 */
 
 /**
@@ -65,6 +123,7 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -73,6 +132,8 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+
+
 
   /* USER CODE END Init */
 
@@ -85,7 +146,22 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+
+	  uint8_t bme280F4config[] = {0xF4,0b00100011};
+	  float temp;
+
+	  if(HAL_I2C_IsDeviceReady(&hi2c1, 0xEC, 2, 30) == HAL_OK){
+		  HAL_GPIO_WritePin(GPIOA, LD2_Pin, 1);
+		  HAL_Delay(100);
+	  }
+
+	  HAL_I2C_Master_Transmit(&hi2c1, 0xEC, bme280F4config, 2, 100);
+
+
+
+
 
   /* USER CODE END 2 */
 
@@ -93,11 +169,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  temp = bme280_temp(&hi2c1,0xEC);
+	  	  HAL_Delay(300);
     /* USER CODE END WHILE */
-	  if(!HAL_GPIO_ReadPin(GPIOC, B1_Pin)){
-		  HAL_GPIO_TogglePin(GPIOA, LD2_Pin);
-		  HAL_Delay(150);
-	  }
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -153,6 +228,54 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10909CEC;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -164,17 +287,11 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
